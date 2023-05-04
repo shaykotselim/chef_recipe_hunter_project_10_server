9d5ba0ecaa69f366d2682f072b563de5964bc55b
shaykot43500 <hossainshaykot@gmail.com>
2023-05-04 18:50:24 +0600

    added recipe

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.Port || 5000;
 
-const chef = require('./data/chef.json')
+const chef = require('./data/chef.json');
+const recipe = require('./data/recipe.json');
+
+app.use(cors());
+
 app.get('/', (app, res)=>{
   res.send('Dragon is running');
 })
 
 app.get('/chef', (req,res)=>{
     res.send(chef)
+})
+app.get ('/recipe', (req, res)=>{
+  res.send(recipe)
+})
+app.get('/recipe/:id', (req,res)=>{
+  const id = req.params.id;
+  console.log(id);
+  const selectRecipe = recipe.find(rec =>rec.chefs_id == id)
+  res.send(selectRecipe)
 })
 app.listen(port, ()=>{
     console.log(`dragon api is runnign on port :${port}`);
